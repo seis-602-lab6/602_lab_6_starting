13c4bfb2f7030e7bab139e0184f2fb103ece0f26
chak1581 <chak1581@stthomas.edu>
2017-10-12 18:24:40 -0500

    My first Commit

--- 602_lab_6_starting/src/RecursiveTrace1.java
+++ 602_lab_6_starting/src/RecursiveTrace1.java
@@ -1,27 +1,27 @@
-
+//changes made
 public class RecursiveTrace1
 {
-	public static int mystery1 (int a, int b)
+	public static int mystery1(int a, int b)
 	{
 		if (a == 0)
 			return 1;
 		else
-			return b * mystery1(a-1,b);
+			return b * mystery1(a - 1, b);
 	}
 
 	public static int equivalent(int a, int b)
 	{
-		return 47; 
+		return 47;
 	}
-	
+
 	public static void main(String[] args)
 	{
 		StdOut.print("Enter a and b, separated by blanks: ");
 		int a = StdIn.readInt();
 		int b = StdIn.readInt();
-		
-		StdOut.printf("mystery1(%d,%d) returns %d.\n",a,b,mystery1(a,b));
-		StdOut.printf("equivalent(%d,%d) returns %d.\n",a,b,equivalent(a,b));
+
+		StdOut.printf("mystery1(%d,%d) returns %d.\n", a, b, mystery1(a, b));
+		StdOut.printf("equivalent(%d,%d) returns %d.\n", a, b, equivalent(a, b));
 
 	}
 
