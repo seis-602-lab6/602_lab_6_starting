c883d77f82d5718d044cecab0ceaac858d127753
SEIS.602.Java <yemm2299@stthomas.edu>
2017-10-28 10:56:37 -0500

    Final commit

--- 602_lab_6_starting/src/RecTurtle.java
+++ 602_lab_6_starting/src/RecTurtle.java
@@ -1,11 +1,33 @@
+import java.awt.Color;
 
 public class RecTurtle
 {
+	private static final int num = 0;
+
 	public static void drawSquare(Turtle t, double side)
 	{
 		// center your square at current turtle location
 		// draw your square, then return turtle to original
 		// location
+		// Nice experience 
+		
+		t.setAngle(90); // point up/north
+		t.penUp();
+		t.goForward(side/2.0);
+		t.penDown();
+		t.turnLeft(90);
+		t.goForward(side/2.0);
+		t.turnLeft(90);
+		t.goForward(side);
+		t.turnLeft(90);
+		t.goForward(side);
+		t.turnLeft(90);
+		t.goForward(side);
+		t.turnLeft(90);
+		t.goForward(side/2.0);
+		t.turnLeft(-90);
+		t.penUp();
+		t.goForward(-side/2.0);
 		
 	}
 
@@ -14,12 +36,49 @@ public class RecTurtle
 		if (num==0)
 			return;
 		
+		ChangeColor(t, num);
+		
 		// finish this...
+		
+		drawSquare(t, initialSide);
+		drawNestedSquares(t, initialSide+sideInc, sideInc, num-1);
+	}
+
+	private static void ChangeColor(Turtle t, int num)
+	{
+		switch (num%3) {
+		case 0: t.setPenColor(Color.BLUE); break;
+		case 1: t.setPenColor(Color.RED); break;
+		case 2: t.setPenColor(Color.GREEN); break;
+		}
+	}
+	public static void spiral(Turtle t, double side, double inc, int num){
+	
+	if (num == 0)
+		return;
+	t.goForward(side);
+	t.turnLeft(90);
+	spiral(t,side+inc, inc, num-1);
 	}
 	
 	public static void main(String[] args)
 	{
+Turtle t = new Turtle (0.0,0.0,0.0);
+t.setPenRadius(0.0025);
+t.setPenColor(Color.RED);
 
+//drawSquare(t, 200);
+
+//drawNestedSquares(t, 50, 20, 20);
+
+t.penDown();
+spiral(t,10,2,200);
+
+switch (num%3) {
+case 0: t.setPenColor(Color.BLUE); break;
+case 1: t.setPenColor(Color.RED); break;
+case 2: t.setPenColor(Color.GREEN); break;
+}
 	}
 
 }
